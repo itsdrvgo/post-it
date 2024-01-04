8d7fe1650bb6fbf23fa17f75e486f16c52c0ddc8
Sarthak Kundu <thedragoluca@gmail.com>
2024-01-04 18:24:43 +0530

    Refactor footer component and add warning message

--- src/components/global/footer/footer.tsx
+++ src/components/global/footer/footer.tsx
@@ -1,28 +1,40 @@
+import { cn } from "@/src/lib/utils";
 import Link from "next/link";
 import { DetailedHTMLProps, HTMLAttributes } from "react";
+import Warning from "../404/warning";
 import Vercel from "../svgs/Vercel";
 
-function Footer({}: DetailedHTMLProps<
-    HTMLAttributes<HTMLElement>,
-    HTMLElement
->) {
+function Footer({
+    className,
+    ...props
+}: DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement>) {
     return (
-        <footer className="flex justify-center border-t border-white/10 p-5">
-            <div className="flex w-full max-w-5xl flex-col items-center justify-center gap-2 md:flex-row md:justify-between">
-                <p className="text-sm text-white/60">
-                    &copy; {new Date().getFullYear()}{" "}
-                    <Link href="https://itsdrvgo.me" className="underline">
-                        DRVGO
-                    </Link>
-                    . All rights reserved.
-                </p>
+        <>
+            <Warning content="WE DO NOT REGULATE THE CONTENT OF THE PAGE." />
 
-                <div className="flex items-center gap-2 text-sm">
-                    <p>Powered by</p>
-                    <Vercel width={70.75} height={16} />
+            <footer
+                className={cn(
+                    "flex justify-center border-t border-white/10 p-5",
+                    className
+                )}
+                {...props}
+            >
+                <div className="flex w-full max-w-5xl flex-col items-center justify-center gap-2 md:flex-row md:justify-between">
+                    <p className="text-sm text-white/60">
+                        &copy; {new Date().getFullYear()}{" "}
+                        <Link href="https://itsdrvgo.me" className="underline">
+                            DRVGO
+                        </Link>
+                        . All rights reserved.
+                    </p>
+
+                    <div className="flex items-center gap-2 text-sm">
+                        <p>Powered by</p>
+                        <Vercel width={70.75} height={16} />
+                    </div>
                 </div>
-            </div>
-        </footer>
+            </footer>
+        </>
     );
 }
 
