a63228ececbfdc1c26a45a270afc5b61b3ef161f
Sarthak Kundu <thedragoluca@gmail.com>
2024-01-04 14:12:24 +0530

    Add rate limiting middleware to TRPC server

--- src/lib/trpc/context.ts
+++ src/lib/trpc/context.ts
@@ -1,17 +1,25 @@
 import { inferAsyncReturnType } from "@trpc/server";
+import { FetchCreateContextFnOptions } from "@trpc/server/adapters/fetch";
 import { db } from "../drizzle";
 import { posts, users } from "../drizzle/schema";
 
-export const createContextInner = () => {
+type ContextProps = {
+    req: Request;
+};
+
+export const createContextInner = ({ req }: ContextProps) => {
     return {
         db,
         posts,
         users,
+        req,
     };
 };
 
-export const createContext = () => {
-    return createContextInner();
+export const createContext = ({ req }: FetchCreateContextFnOptions) => {
+    return createContextInner({
+        req,
+    });
 };
 
 export type Context = inferAsyncReturnType<typeof createContextInner>;
--- src/lib/trpc/trpc.ts
+++ src/lib/trpc/trpc.ts
@@ -1,7 +1,11 @@
-import { initTRPC } from "@trpc/server";
+import { initTRPC, TRPCError } from "@trpc/server";
 import superjson from "superjson";
 import { ZodError } from "zod";
 import { Context } from "./context";
+
+const map = new Map<string, number[]>();
+const MAX_REQUESTS = 100;
+const WINDOW_SIZE = 60 * 1000;
 
 export const t = initTRPC.context<Context>().create({
     transformer: superjson,
@@ -19,5 +23,37 @@ export const t = initTRPC.context<Context>().create({
     },
 });
 
+const ratelimiter = t.middleware(({ ctx, next }) => {
+    const ip =
+        ctx.req.headers.get("x-real-ip") ??
+        ctx.req.headers.get("x-forwarded-for") ??
+        "";
+
+    if (ip === "")
+        throw new TRPCError({
+            code: "INTERNAL_SERVER_ERROR",
+            message: "No IP address found",
+        });
+
+    const now = Date.now();
+    const requests = map.get(ip) ?? [];
+
+    while (requests.length > 0 && requests[0] < now - WINDOW_SIZE)
+        requests.shift();
+
+    if (requests.length >= MAX_REQUESTS)
+        throw new TRPCError({
+            code: "TOO_MANY_REQUESTS",
+            message: "Too many requests",
+        });
+
+    requests.push(now);
+    map.set(ip, requests);
+
+    return next({
+        ctx,
+    });
+});
+
 export const createTRPCRouter = t.router;
-export const publicProcedure = t.procedure;
+export const publicProcedure = t.procedure.use(ratelimiter);
